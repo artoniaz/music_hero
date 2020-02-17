634f4fa252d172de1b56ad6653d0ceb3e16eebb2
artoniaz <artoni.az@gmail.com>
2020-02-17 11:00:52 +0100

    Updates

---
+++ precache-manifest.ae51491477ec6b3c4d6ae12079b3e698.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6d0d5f662efb23fa1f3ae4104c22722",
+    "url": "/music_hero/index.html"
+  },
+  {
+    "revision": "3ef7693b3d0c4858ae35",
+    "url": "/music_hero/static/css/main.67d0366d.chunk.css"
+  },
+  {
+    "revision": "7c0cbe4d54e1257e880f",
+    "url": "/music_hero/static/js/2.75a6b91b.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/music_hero/static/js/2.75a6b91b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ef7693b3d0c4858ae35",
+    "url": "/music_hero/static/js/main.35889d15.chunk.js"
+  },
+  {
+    "revision": "18becabe57f10b5272fb",
+    "url": "/music_hero/static/js/runtime-main.71ef662b.js"
+  },
+  {
+    "revision": "7130a192b92d91320292acb6877b3e9f",
+    "url": "/music_hero/static/media/ladyGuitar.7130a192.png"
+  }
+]);
